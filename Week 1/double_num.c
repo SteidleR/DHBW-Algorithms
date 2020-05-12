676d1fe35374d370c5fe63c046f5a4fd0021cebf
SteidleR <rroosstt008@gmail.com>
2020-05-12 22:05:02 +0200

    double_num.c | reverse_str.c: 0.1 (Init Docs)

---
+++ Week 1/double_num.c
@@ -0,0 +1,11 @@
+/*
+Title: Double Numbers
+ShortSum: Double the Numbers in a file
+Author: Robin Steidle
+Version: 0.1
+LastEdit: -
+*/
+
+void main() {
+
+}
---
+++ Week 1/reverse_str.c
@@ -0,0 +1,11 @@
+/*
+Title: Reverse String
+ShortSum: Reverse the String readed from file
+Author: Robin Steidle
+Version: 0.1
+LastEdit: -
+*/
+
+void main() {
+    
+}
