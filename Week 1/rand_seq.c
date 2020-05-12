518e0fad8ac933dbc062758134ed8e40df09f4f8
SteidleR <rroosstt008@gmail.com>
2020-05-12 17:05:11 +0200

    rand_seq.c; 0.1 (Doc init)

---
+++ Week 1/rand_seq.c
@@ -0,0 +1,7 @@
+/*
+Title: Random Sequence Generator
+ShortSum: Generates a sequence of random numbers between 0 (included) and MAXNUM (excluded)
+Author: Robin Steidle
+Version: 0.1
+LastEdit: 12.05.2020
+*/
