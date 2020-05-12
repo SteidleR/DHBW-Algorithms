5bd0690a933ab9dfa4a017c61bfc4f79d4734e40
SteidleR <rroosstt008@gmail.com>
2020-05-12 17:35:03 +0200

    rand_seq.c: 0.2 (Core Functions)

--- Week 1/rand_seq.c
+++ Week 1/rand_seq.c
@@ -5,3 +5,21 @@ Author: Robin Steidle
 Version: 0.1
 LastEdit: 12.05.2020
 */
+
+#define MAXNUM 1000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main() {
+    // for more randomness: use time as seed
+    srand(time(0));
+
+    int n=10; // Length of sequence
+
+    for (int i=0; i<=n; i++) {
+        int num = rand()%MAXNUM;
+        printf("%d\n", num);
+    }
+}
