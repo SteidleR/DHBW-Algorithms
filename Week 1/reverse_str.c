95ead18410598162da4e8e20912f2587bf1230e4
SteidleR <rroosstt008@gmail.com>
2020-05-13 15:50:24 +0200

    reverse_str.c: 0.2 (Str from stdin)

--- Week 1/reverse_str.c
+++ Week 1/reverse_str.c
@@ -2,10 +2,46 @@
 Title: Reverse String
 ShortSum: Reverse the String readed from file
 Author: Robin Steidle
-Version: 0.1
-LastEdit: -
+Version: 0.2
+LastEdit: 13.05.2020
 */
 
-void main() {
-    
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char*);
+
+void main(int argc, char *argv) {
+    if (argc > 2) {
+        // to many Arguments
+        printf("[ERROR]Expected 1 Argument, got %d!\n", argc-1);
+        exit(EXIT_FAILURE);
+    } else if (argc == 1) {
+        // Use stdin PIPE
+        FILE *instream = fopen("/dev/stdin","r");
+        char chunk[256];
+        while(fgets(chunk,sizeof(chunk), instream) != NULL) {
+            int len = strlen(chunk);
+            chunk[len-1] = *"";
+            printf("%s: ",chunk);
+            reverse(chunk);
+            printf("%s\n",chunk);
+        }
+    } else {
+        
+    }
+}
+
+void reverse(char *Str) {
+    char temp;
+    int i=0, j=0;
+    j = strlen(Str)-1;
+    while (i<j) {
+        temp = Str[i];
+        Str[i] = Str[j];
+        Str[j] = temp;
+        i++;
+        j--;
+    }
 }
