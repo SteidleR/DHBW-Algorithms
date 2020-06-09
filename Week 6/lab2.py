429ea867323728db8b3a645a1abfba11f43c9605
Steidle <inf9152@lehre.dhbw-stuttgart.de>
2020-06-09 11:31:37 +0200

    added docstrings

--- Week 6/lab2.py
+++ Week 6/lab2.py
@@ -1,20 +1,28 @@
-import copy
+app_info = {
+"Title": "Lab Solver",
+"ShortSum": "Solves a lab readed from file",
+"LongDescr": "Solves a labyrinth using the Depth-First Order algorithm. Labyrinth is readed from a file. Walls of the labyrinth are #",
+"Author": "Robin Steidle",
+"Version": "0.4",
+"LastEdit": "08.06.2020"
+}
+
 import time
 import os
 
-labname = ["testlabtheseus.txt", "testlab2.txt", "lab.txt"][0]
+# Available test labyrinths to solve
+labname = ["testlabtheseus.txt", "testlab2.txt", "lab.txt", "lab_big.txt"][0]
 stepName = {"U": [0, -1], "D": [0, 1], "R": [1, 0], "L": [-1, 0]}
 
-# Coords of Startpoint
-sx = None
-sy = None
-
-# Coords of Endpoint
-ex = None
-ey = None
-
 def readLab(fname):
-    global sx, sy, ex, ey
+    """
+    Reads the Labyrinth from file
+
+    :param fname: File name of the labyrinth
+    :return:      2D array storing the labyrinth
+                  Start Point [x,y]
+                  End Point [x,y]
+    """
     arr = []
     count = 0
     with open(fname) as f:
@@ -27,21 +35,42 @@ def readLab(fname):
                 ey = count
             arr.append(list(line.replace("\n", "")))
             count += 1
-    return arr
+    return arr, [sx, sy], [ex, ey]
 
 def printLab(arr):
     for l in arr:
         print("".join(l))
 
-def printPath(path, labn):
-    for i in range(1, len(path)-1):
+def printPath(path, lab):
+    for i in range(len(path)):
         p = path[i]
-        labn[p[1]][p[0]] = "."
-    printLab(labn)
+        lab[p[1]][p[0]] = "\u001b[47m\u001b[34m{}\u001b[0m".format(lab[p[1]][p[0]])
+    printLab(lab)
+
+def calculateCost(path, lab):
+    cost = 0
+    for p in path:
+        char = lab[p[1]][p[0]]
+        if char == " ":
+            cost += 1
+        elif char == "S" or char == "X":
+            cost += 1
+        elif char in "0123456789":
+            cost += int(char)
+    return cost
 
 # ----------- Functions to solve the Lab ----------------
 
 def checkNeighbours(p, pp, lab, path):
+    """
+    Checks all available points to walk from current point
+
+    :param p:    current point
+    :param pp:   previous point
+    :param lab:  labyrinth
+    :param path: current path
+    :return:     Array of points to walk
+    """
     x = p[0]
     y = p[1]
     px = pp[0]
@@ -49,24 +78,32 @@ def checkNeighbours(p, pp, lab, path):
     nextp = []
     for key, val in stepName.items():
         np = [x+val[0], y+val[1]]
-        if px-val[0] == x and py-val[1] == y:
+        if lab[np[1]][np[0]] == "#":
             continue
-        try:
-            path.index(np)
+        elif np == pp:
             continue
-        except:
-            if lab[np[1]][np[0]] != "#":
-                nextp.append(np)
+        elif np in path:
+            return None
+        if lab[np[1]][np[0]] != "#":
+            nextp.append(np)
     return nextp
 
 def solve(lab, sp, ep):
+    """
+    Solves the Labyrinth
+
+    :param lab: 2D Array which stores the labyrinth to solve
+    :param sp:  Starting Point [x,y]
+    :param ep:  End Point [x,y]
+    :return:    An Array of all paths leading to the end point
+    """
     solvedPaths = []
     towalkPaths = []
     currentPath = [sp]
     point = sp
     while True:
         while True:
-            steps = checkNeighbours(point, currentPath[-1], lab, currentPath)
+            steps = checkNeighbours(point, currentPath[-2 if len(currentPath)>1 else -1], lab, currentPath)
             if not steps:
                 break
             point = steps.pop(0)
@@ -88,9 +125,11 @@ def solve(lab, sp, ep):
 
 if __name__ == "__main__":
     tstart = time.time()
-    lab  = readLab(labname)
-    paths = solve(lab, [sx,sy], [ex,ey])
+    lab, sp, ep  = readLab(labname)
+    paths = solve(lab, sp, ep)
     tend = time.time()
-    paths.sort(key= lambda i: len(i))
+    paths.sort(key= lambda i: calculateCost(i, lab))
+    cost = calculateCost(paths[0], lab)
     printPath(paths[0], lab)   
-    print("Time needed: ", tend - tstart)
+    print("Cost: ",cost)
+    print("Time needed to solve: ", tend - tstart)
--- Week 6/lab_dia.py
+++ Week 6/lab_dia.py
@@ -2,19 +2,9 @@ import copy
 import time
 import os
 
-labname = ["testlabtheseus.txt", "testlab2.txt", "lab.txt"][0]
+labname = ["testlabtheseus.txt", "testlab2.txt", "lab.txt", "lab_big.txt"][2]
 stepName = {"U": [0, -1], "D": [0, 1], "R": [1, 0], "L": [-1, 0], "UR": [1, -1], "UL": [-1, -1], "DR": [1, 1], "DL": [-1, 1]}
-
-# Coords of Startpoint
-sx = None
-sy = None
-
-# Coords of Endpoint
-ex = None
-ey = None
-
 def readLab(fname):
-    global sx, sy, ex, ey
     arr = []
     count = 0
     with open(fname) as f:
@@ -27,21 +17,35 @@ def readLab(fname):
                 ey = count
             arr.append(list(line.replace("\n", "")))
             count += 1
-    return arr
+    return arr, [sx, sy], [ex, ey]
 
 def printLab(arr):
     for l in arr:
         print("".join(l))
 
-def printPath(path, labn):
-    for i in range(1, len(path)-2):
+def printPath(path, lab):
+    for i in range(1, len(path)-1):
         p = path[i]
-        labn[p[1]][p[0]] = "."
-    printLab(labn)
+        lab[p[1]][p[0]] = "\u001b[47m\u001b[34m{}\u001b[0m".format(lab[p[1]][p[0]])
+    printLab(lab)
+
+def calculateCost(path, lab):
+    cost = 0
+    for p in path:
+        char = lab[p[1]][p[0]]
+        if char == " ":
+            cost += 1
+        elif char == "S" or char == "X":
+            cost += 1
+        elif char in "0123456789":
+            cost += int(char)
+        else:
+            raise ValueError("An unknown Cost is found!")
+    return cost
 
 # ----------- Functions to solve the Lab ----------------
 
-def checkNeighbors(p, pp, lab, path):
+def checkNeighbours(p, pp, lab, path):
     x = p[0]
     y = p[1]
     px = pp[0]
@@ -49,106 +53,52 @@ def checkNeighbors(p, pp, lab, path):
     nextp = []
     for key, val in stepName.items():
         np = [x+val[0], y+val[1]]
-        if px-val[0] == x and py-val[1] == y:
+        if lab[np[1]][np[0]] == "#":
             continue
-        try:
-            path.index(np)
+        elif np == pp:
             continue
-        except:
-            if lab[np[1]][np[0]] != "#":
-                nextp.append(key)
+        
+        elif np in path:
+            return None
+        
+        if lab[np[1]][np[0]] != "#":
+            nextp.append(np)
     return nextp
 
-def walk(p, step):
-    p_new = [p[0] + stepName[step][0], p[1] + stepName[step][1]]
-    return p_new
-
-
-def checkBetter(x,y, path, solvedPaths, lab):
-    worsePaths = []
-    isBetter = False
-    for i in range(len(solvedPaths)):
-        sp = solvedPaths[i]
-        try:
-            #lensp = sp.index([x,y])
-            #lenpath = len(path)
-            lensp = calcCosts(lab, sp[:sp.index([x,y])])
-            lenpath = calcCosts(lab, path)
-            if lenpath > lensp:
-                isBetter = True
-            elif lenpath < lensp:
-                worsePaths.append(i)
-        except:
-            continue
-    return isBetter, worsePaths
-
-def calcCosts(lab, path):
-    costs = 0
-    for p in path:
-        if p == True:
-            break
-        currentPoint = lab[p[1]][p[0]]
-        if currentPoint == " ":
-            costs += 1
-        elif currentPoint == "S" or currentPoint == "X":
-            costs += 1
-        else:
-            costs += int(lab[p[1]][p[0]])
-    return costs
-
-def solveLab(x, y, lab):
+def solve(lab, sp, ep):
     solvedPaths = []
-    currentPath = [[sx,sy]]
-    pendingPaths = []
-    worsePath = []
+    towalkPaths = []
+    currentPath = [sp]
+    point = sp
     while True:
         while True:
-            # Check if Path is at Endpoint
-            if x==ex and y==ey:
-                currentPath.append(True)
+            steps = checkNeighbours(point, currentPath[-2 if len(currentPath)>1 else -1], lab, currentPath)
+            if not steps:
+                break
+            point = steps.pop(0)
+            for step in steps:
+                towalkPaths.append([*currentPath, step])
+            currentPath.append(point)
+            if point == ep:
                 solvedPaths.append(currentPath)
-                if worsePath:
-                    worsePath = list(dict.fromkeys(worsePath))
-                    worsePath.sort()
-                    for i in worsePath[::-1]:
-                        del solvedPaths[i]
                 break
-
-            # Get Available steps to walk
-            steps = checkNeighbors(currentPath[-1], currentPath[-1 if len(currentPath)==1 else -2], lab, currentPath)
-            
-            if len(steps) > 1:
-                for i in range(1, len(steps)):
-                    pendingPaths.append([*currentPath, walk(currentPath[-1], steps[i])])
-            elif len(steps)==0:
+            if point in currentPath[:-1]:
                 break
-            [x,y] = walk(currentPath[-1], steps[0])
-
-            # Check if other path is faster
-            checkB, wPath = checkBetter(x,y, currentPath, solvedPaths, lab)
-            if wPath != []:
-                worsePath = [*worsePath, *wPath]
-            if checkB:
-                break
-
-            currentPath.append([x,y])
-        worsePath = []
-        if len(pendingPaths):
-            currentPath = pendingPaths.pop(0)
-            [x,y] = currentPath[-1]
+        
+        if towalkPaths:
+            currentPath = towalkPaths.pop()
+            point = currentPath[-1]
         else:
             break
     return solvedPaths
 
-
-# ----------------------------------------------------------
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     tstart = time.time()
-    lab = readLab(labname)
-    paths = solveLab(sx, sy, lab)
+    lab, sp, ep  = readLab(labname)
+    paths = solve(lab, sp, ep)
     tend = time.time()
-    paths.sort(key=lambda s: len(s))
-    print("Cost: ", calcCosts(lab, paths[0]))
-    printPath(paths[0], lab)
+    paths.sort(key= lambda i: calculateCost(i, lab))
+    cost = calculateCost(paths[0], lab)
+    printPath(paths[0], lab)   
+    print("Cost: ",cost)
     print("Time needed to solve: ", tend - tstart)
